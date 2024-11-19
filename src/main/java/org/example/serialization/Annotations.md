6ca0e9e1f8e70547ec7eaad0e8b0de16ec99ec78
Luis Almeida <anjos.luiseduardo@gmail.com>
2024-11-19 03:07:59 -0300

    feature(serialization): Entendendo serializacao

---
+++ src/main/java/org/example/serialization/Annotations.md
@@ -0,0 +1,4 @@
+# Serialização
+ - É uma tecnica que permite representar objetos em uma *stream de bytes*(Sequencia)
+ - Ajuda na persitência e transmissão desses objetos, além de fazer uma cópia exata do mesmo.
+ - Classes tem que *implementar a interface Serializable* para permitirem serialização.
--- src/main/java/org/example/serialization/Person.java
+++ src/main/java/org/example/serialization/Person.java
@@ -1,4 +1,67 @@
 package org.example.serialization;
 
-public class Person {
+import java.io.*;
+
+public class Person implements Serializable {
+    private static final Long serialVersionUID = 1L; //Lembrando que atributos static pertencem a classe e não ao objeto, sendo assim nao serializaveis
+    static String country = "ITALY";
+    private Integer age;
+    private String name;
+    transient Integer height; //Transient é usado para ignorar atributos durante a serializacao.
+
+
+    public void serialization() throws IOException, ClassNotFoundException {
+        Person person = new Person();
+        person.setAge(20);
+        person.setName("Joe");
+
+        FileOutputStream fileOutputStream = new FileOutputStream("yourfile.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream); //Salva o estado do objeto serializado.
+        objectOutputStream.writeObject(person);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream("yourfile.txt");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream); //Deserializa a stream e tranforma em objetonovamente
+        Person p2 = (Person) objectInputStream.readObject();
+        objectInputStream.close();
+
+        assertTrue(p2.getAge() == person.getAge());
+        assertTrue(p2.getName().equals(person.getName()));
+    }
+
+    private void assertTrue(boolean b) {
+    }
+
+    public static String getCountry() {
+        return country;
+    }
+
+    public static void setCountry(String country) {
+        Person.country = country;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
 }
